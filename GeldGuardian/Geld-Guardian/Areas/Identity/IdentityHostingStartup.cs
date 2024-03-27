eaa9df23f5140726b5a3d656c7608da0126efb89
if23b234 <49563497+sdahlke5083@users.noreply.github.com>
2024-03-27 23:40:56 +0100

    Minor Changes for easier understanding. Added - scaffolding files for Login, Register, AccessDenied - these files will be needed to apply any changes to the login handler. More to be added as neeeded. - let EFCore run for create UserIdentity tables.
    Modified
    - Stock Files Counter, NavMenu, SurveyPrompt and MainLayout and added some descriptive texts

---
+++ GeldGuardian/Geld-Guardian/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Geld_Guardian.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Geld_Guardian.Areas.Identity.IdentityHostingStartup))]
+namespace Geld_Guardian.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
