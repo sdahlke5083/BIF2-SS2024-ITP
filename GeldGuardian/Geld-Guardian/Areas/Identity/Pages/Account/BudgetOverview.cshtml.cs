b2325ddb97ef04192baab8ebbe1b6caac9997bee
Soppphie <if23b290@technikum-wien.at>
2024-04-23 15:10:37 +0200

    Basics added, columns and comment section

---
+++ GeldGuardian/Geld-Guardian/Areas/Identity/Pages/Account/BudgetOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Geld_Guardian.Areas.Identity.Pages.Account
+{
+    public class BudgetOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
